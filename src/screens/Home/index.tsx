eed1334f93933661b1d795f26b4b071135ae307b
Rafael Domingues <r-domingues@outlook.com>
2022-09-16 20:51:44 -0300

    finished home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import { View, Image } from 'react-native';
+import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
+import { CardGame } from '../../components/CardGame';
+import { Heading } from '../../components/Heading';
 
 import { styles } from './styles';
+import { GAMES } from '../../utils/games'
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Image source={logoImg} style={styles.logo}/>
+      <Image source={logoImg} style={styles.logo} />
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar.."
+      />
+      <FlatList
+        data={GAMES}
+        contentContainerStyle={styles.contentList}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <CardGame
+            data={item}
+          />
+        )}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
     </View>
   );
 }
